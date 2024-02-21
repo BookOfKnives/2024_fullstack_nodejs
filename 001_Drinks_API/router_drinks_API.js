809681a79f76efbf59214a3cffa7a7a23b732359
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-02-21 18:29:09 +0100

    Added Drinks POST, still needs complete body check.

--- 001_Drinks_API/router_drinks_API.js
+++ 001_Drinks_API/router_drinks_API.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const data = require("./drinks_data.js");
 router.use(express.json());
 
+const newDataEntryIdNumber = { newIdNumber: 0, };
+data.drinks.map((drink) => {
+    if (newDataEntryIdNumber.newIdNumber < drink.id) {
+        newDataEntryIdNumber.newIdNumber = drink.id;
+    }
+});
+
+// console.log("001 drink api says, this is my newIdNumber:::", newDataEntryIdNumber.newIdNumber);
+
 console.log("001 router drinks API online.");
 
 router.get("/drinks", (req, res) => {
@@ -18,5 +27,23 @@ router.get("/drinks/:idnumber", (req, res) => {
         res.send(foundDrink);
     }
 });
+
+router.post("/drinks", (req, res) => {
+    const idNumberForNewEntry = ++newDataEntryIdNumber.newIdNumber;
+    const newDrink = {
+        id: idNumberForNewEntry,
+        name: req.body.name,
+        price: req.body.price, 
+        color: req.body.color, 
+        percentage: req.body.percentage,
+    };
+    if (!newDrink.name) {
+        res.status(400).send(`Invalid POST format, please submit name (string), 
+        price (number), color (string) and percentage (number).`)
+    } else {
+        data.drinks.push(newDrink);
+        res.json(newDrink);    
+    }
+})
 
 module.exports = router;
