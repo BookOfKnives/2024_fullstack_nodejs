73a372a68fd36374ea212d3ed1f91b2a603dd8a2
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-02-21 17:05:03 +0100

    made server, router, drinks_data in CJS format

---
+++ 001_Drinks_API/drinks_data.js
@@ -0,0 +1,22 @@
+exports.data = {
+    drinks: [
+        {
+            name: "martini",
+            price: 100,
+            color: "orange",
+            percentage: 35,
+        },
+        {
+            name: "vodka",
+            price: 25,
+            color: "clear",
+            percentage: 80,
+        },
+        {
+            name: "lemon-lime",
+            price: 45,
+            color: "yellow and green",
+            percentage: 0,
+        }
+    ]
+};
--- 001_Drinks_API/server.js
+++ 001_Drinks_API/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const drinks_router = require("./router_drinks_API");
+
+app.use("/api", drinks_router); 
 
 const PORT = 8080;
 app.listen(
     PORT, 
-    () => { console.log("001 drinks api server running on", PORT) }
+    () => { console.log("001 server running on port:", PORT) }
 );
