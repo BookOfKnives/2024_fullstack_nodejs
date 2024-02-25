24819c08ecf319b75a46af4443fb0d0b0acfc02d
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-02-25 16:13:41 +0100

    made homepage, gitignore, express path

---
+++ 002_time_vercel/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/homepage/homepage.html")
+})
+
+app.listen(PORT, () => {console.log("002 time vercel server listening on port:", PORT)});
