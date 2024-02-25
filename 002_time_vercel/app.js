cc60f72aa5de327492d1dbdbe7ef26690f635371
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-02-25 21:35:06 +0100

    changed port to 5000 from 8080

--- 002_time_vercel/app.js
+++ 002_time_vercel/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = 8080;
+const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/homepage/homepage.html")
