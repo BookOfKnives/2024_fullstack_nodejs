bc8624de54d683cc34b3450819f60e188cb5ddf5
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-02-25 21:33:56 +0100

    added test path to /2

--- 002_time_vercel/app.js
+++ 002_time_vercel/app.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/homepage/homepage.html")
 })
 
+app.get("/1", (req, res) => {
+    res.send({ data: "welcome to 002, time. "})
+});
+
 app.listen(PORT, () => {console.log("002 time vercel server listening on port:", PORT)});
 
 module.exports = app;
