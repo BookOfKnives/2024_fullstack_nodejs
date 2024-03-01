df070a14c3f9d72481565c208bd460ff55760d13
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-03-01 11:09:24 +0100

    DogTinder homepage, matches

--- 003_DogInder/app.js
+++ 003_DogInder/app.js
@@ -6,8 +6,26 @@ app.use(express.static("public"));
 
 import path from "path";
 
+import getMatches from "./public/matches/matches.js";
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("public/homepage/homepage.html"));
+});
+app.get("/proxy", (req, res) => {
+    fetch("https://www.google.com")
+    .then(response => response.text())
+    .then(result => res.send(result));
+});
+
+app.get("/matches", (req, res) => {
+    res.sendFile(path.resolve("public/matches/matches.html"));
+})
+
+//=======API
+
+app.get("/api/matches", async (req, res) => {
+    const matches = getMatches();
+    res.send({ data: matches });
 });
 
 const PORT = 8080;
