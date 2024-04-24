d6603f4916a3fe5f40a08a98c0a00a0f2928b75f
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-04-24 15:39:07 +0200

    CSS grid, auth

--- 012_manda2_ssr/frontend/src/App.svelte
+++ 012_manda2_ssr/frontend/src/App.svelte
@@ -1,8 +1,40 @@
 <script>
+    import { Router, Link, Route } from "svelte-navigator";
+    import Home from "./pages/Home/Home.svelte";
+    import Auth from "./pages/Auth/Auth.svelte";
 
 </script>
 
-<h1>WElcome to manda2 012</h1>
+<main>
+    <div id="front-container-div">
+        <div class="nav-bar-div">
+            <Router>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="Auth">Auth</Link>
+                </nav>
+                <div>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+
+                </div>
+            </Router>
+        </div>
+        <div class="page-header-div">            
+            <h1>Welcome to 1618 Design </h1>
+        </div>
+        <div>
+
+        </div>
+        <div class="auth-">
+            <Auth />
+        </div>
+        <div>
+            <p>This is a text of intro.</p>
+        </div>
+    </div>
+</main>
 
 <style>
 
---
+++ 012_manda2_ssr/frontend/src/pages/Auth/Auth.svelte
@@ -0,0 +1,66 @@
+<script>
+    let newUsername = "";
+    let newPassword = "";
+
+    let username = "";
+    let password = "";
+    /*
+      // Function to get a cookie value
+      function getCookie(name) {
+        const cookies = document.cookie.split(';');
+        for (let cookie of cookies) {
+          const [cookieName, cookieValue] = cookie.split('=');
+          if (cookieName.trim() === name) {
+            return cookieValue;
+          }
+        }
+        return "";
+      }
+    
+      function getAllCookie() {
+        return  document.cookie;
+    
+      }
+    */
+
+    async function hitAuthenticate() {
+        let loginData = {
+            username,
+            password,
+        }
+        const data = await fetch("http://localhost:8080/authenticate/", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        });
+    }
+    
+    async function hitSignup() {
+        let signupData = {
+            newUsername,
+            newPassword,
+        }
+        const response = await fetch("http://localhost:8080/signup/", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(signupData),
+        });}
+
+</script>
+
+<label for="username">Username</label>
+<input type=text bind:value={username} name="username" id="username" placeholder="Username">
+<label for="password">Password</label>
+<input type=text bind:value={password} name="password" id="password" placeholder="Password">
+<button on:click={hitAuthenticate}>Hit authenticate</button>
+<label for="newUserName">New User name</label>
+<input type=text bind:value={newUsername} name="newusername" id="newusername" placeholder="New User Name">
+<label for="newUserPassword">New User password</label>
+<input type=text bind:value={newPassword} name="newuserpassword" id="newuserpassword" placeholder="New password">
+<button on:click={hitSignup}>Signup</button>
