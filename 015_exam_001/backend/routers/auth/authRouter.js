325d746bec97f0031920e870f8605a0117d9a149
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-05-17 15:24:23 +0200

    cl refactor

--- 015_exam_001/backend/routers/auth/authRouter.js
+++ 015_exam_001/backend/routers/auth/authRouter.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { createUser } from "../../database/users/createUser.js"
 import { getUser } from "../../database/users/getUser.js";
 import mailer from "../../util/mailer.js";
+import { cl, cle } from "../../util/logger.js";
 
 const router = Router();
 const startUpMessage = "Auth Router online.";
@@ -21,13 +22,13 @@ router.post("/newusersignup", async (req, res) => {
         signUpDate: new Date().toLocaleString("da-DK", {timeZone: "Europe/Copenhagen"}),
         lastLogon: null,
     };
-    createUser(newUser);
+    createUser(newUser); //egegntlig bør authRouter IKKE lave ting -- den bør sende det til userApi
     req.session.user = newUser;
     try {
-        let newUserEmail = "hans079d@stud.kea.dk";
+        let newUserEmail = newUser.email;
         mailer(newUserEmail) 
     } catch (err) {
-        console.error("authRouter error in mailing, error:", err);
+        cle(err, "authRouter error in mailing, error:");
     }
     res.status(200).send({ data: newUser });
     
@@ -40,14 +41,14 @@ router.post("/login", async (req, res) => {
     try {
         dbLookup = await getUser(req.body);
     } catch (err) {
-        console.error("login in authRouter gives error on dbLookUp, error:", err);
+        cle(err, "login in authRouter gives error on dbLookUp, error:");
     }
 
     let pwCheck = false;
     try {
         pwCheck = await passwordUtil.compare(signInattempt.password, dbLookup.user.password);
     } catch (err) {
-        console.error("pwcheck in authRouter gives error on pwCheck, error:", err);
+        cle(err, "pwcheck in authRouter gives error on pwCheck, error:");
     }
     if (pwCheck) { 
         req.session.userAuthenticated = true;
