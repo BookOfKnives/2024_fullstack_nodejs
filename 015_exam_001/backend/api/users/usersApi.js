ba32030f7a0be4ae81b4183313472e274490c48d
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-05-17 15:21:57 +0200

    changed to use cl instead of console log

--- 015_exam_001/backend/api/users/usersApi.js
+++ 015_exam_001/backend/api/users/usersApi.js
@@ -1,3 +1,6 @@
+let debug = process.argv.includes("debug");
+import { cl } from "../../util/logger.js";
+
 import { Router } from "express";
 const router = Router();
 const startUpMessage = "Users API Router online.";
@@ -17,19 +20,17 @@ import {getAllUsers} from "../../database/users/getAllUsers.js";
  }
 */
 
-//inputChecker
-
-
 router.get(routerUrl, async (req, res) => {
     const data = await getAllUsers();
     res.send({ data })
-})
+});
 
 router.post(routerUrl, (req, res) => {
     const user = req.body;
-
+    if (debug) { cl("usersApi RouterPost says, USER from req.body:", user); }
+    createUser(user);
     res.send("user api router post somsome user")
-})
+});
 
 export default router;
 
