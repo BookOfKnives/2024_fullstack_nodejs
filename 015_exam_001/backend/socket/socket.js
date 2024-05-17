7b337ea9e179fc75bba815ef2b7667789403925e
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-05-17 15:24:54 +0200

    socket setup and cl refactor, middleware

--- 015_exam_001/backend/socket/socket.js
+++ 015_exam_001/backend/socket/socket.js
@@ -1,7 +1,11 @@
-let debug = false;
-function debugSocket(debugSet) {
-  debug = debugSet;
-};
+let debug = process.argv.includes("debug");
+import { cl } from "../util/logger.js";
+// import //skal have session middleware her??
+
+let user;
+function setSessionUser(sessionUser) {
+  user = sessionUser; //bruig en fetch her, methinks. tjek om det virker med at fetche.
+}
 
 let krydsOgBolle = {
   0: undefined, 
@@ -14,18 +18,38 @@ let krydsOgBolle = {
   7: undefined,
   8: undefined,
 }
+
+function ioSetup(io, sessionMiddleware) {
+  setupSocket(io);
+  // chatSocket(io);
+  io.engine.use(sessionMiddleware);
+}
+//socket.request.session.nickname for at tilgå session i  socketen
 function setupSocket(io) {
   io.on('connection', (client) => {
-    if (debug) { console.log("connection in socketjs being hit") };
+    if (debug) { cl("connection in socketjs being hit, client id:", client.id) };
     io.emit("onConnectionSendBoardState", krydsOgBolle); 
     
     client.on("transmitGameBoardUserChoice", (...args) => {
-      krydsOgBolle[args[0]] = args[1]; // clear this
-      io.emit("serverTransmitGameBoardUpdate", krydsOgBolle); //clear this
-      if (debug) { console.log("serverTransmitGameBoardUpdate in socketjs being hit") };
+      krydsOgBolle[args[0]] = args[1];
+      io.emit("serverTransmitGameBoardUpdate", krydsOgBolle); 
+      if (debug) { cl("serverTransmitGameBoardUpdate in socketjs being hit, client id:", client.id) };
     });
 
   });
 };
+/* 
+function chatSocket(io) {
+  io.on("connection", (client) => {
+    if (debug) { cl("chatSocket connection in socketjs being hit, socket name:", client.id); };
+    client.on("chatMessageSentFromUser", (args) => {
+      if (debug) { cl("chatMessageSentFromUser connection in socketjs being hit, args, client id", args, client.id) };
+      //unfininshed
+    });
+  });
+}
+ */
+// alt shift A for at udkommentere
 
-export {setupSocket, debugSocket};
+// export {setupSocket, chatSocket, setSessionUser};
+export {ioSetup, setSessionUser};
