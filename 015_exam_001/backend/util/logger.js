ac0d7401e012e6b9825edac2ec308422c999dd19
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-05-17 15:25:46 +0200

    removed time, added chalk logger, startupmsg clean-up

---
+++ 015_exam_001/backend/util/logger.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+
+let timeOptions = {
+    timeZone: "Europe/Copenhagen",
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+}
+
+export function cl(...text) {
+    let a, b, rest;
+    [a, b, rest] = text
+    console.log(chalk.blue(new Date().toLocaleString("da-DK", timeOptions)), a ?? chalk.hidden(null), chalk.yellow(b) ?? chalk.hidden(null), rest ?? chalk.hidden(null));
+}
+
+export function cle(error, ...text) {
+    let a, b, rest;
+    [a, b, rest] = text
+    console.error(chalk.blue(new Date().toLocaleString("da-DK", timeOptions)), a ?? chalk.hidden(null), chalk.yellow(b) ?? chalk.hidden(null), rest ?? chalk.hidden(null), chalk.red("Error!"), error);
+}
