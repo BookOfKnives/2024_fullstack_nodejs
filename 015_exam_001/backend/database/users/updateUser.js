bbf8b2836b664f23ec95c630d22248c9139572e5
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-05-28 21:47:36 +0200

    added func of patch

--- 015_exam_001/backend/database/users/updateUser.js
+++ 015_exam_001/backend/database/users/updateUser.js
@@ -1,12 +1,21 @@
 import { userColl } from "../connection.js";
 
-export async function updateUserLastLogonTime(user, time) {
-    // let userTemp = await userColl.findOne({"user.username":user.name});
-    //user updateONe{}
-    let result = await userColl.updateOne({"user.username":user.name}, {$set: {"user.lastLogon": time}} )
+export async function updateUserLastLogonTimeId(id, time) { 
+    let result = await userColl.updateOne({"user.id":id}, {$set: {"user.lastLogon": time}} )
     console.log("updateuserlastlogontime, result:", result);
-    //hvad er syntaks for at ændre noget?
-    // userTemp.lastLogon = time;
-    //nej ... jeg skal jo modifere den -- så jeg skal have DML
-    return "OK in updatelastuserlogontime";
+    return result;
+}
+
+export async function updateUserEmail(id, email) { //TODO -- consider to remove if patch works
+    let result = await userColl.updateOne({"user.id":id}, {$set: {"user.email": email}});
+    console.log("update email result:", result);
+    return "ok maybe in update user email in updateuser.js";
+}
+
+export async function patchUser(id, userPatchData) { //TODO test that this replaces the user data in the db
+    let result = await userColl.replaceOne({"user.id":id}, {$set: {"user": userPatchData}});
+    console.log("pathchuser in updateUIser js", result);
+    return "maybe OK in patch";
+    // db.coll.replaceOne({"name": "Max"}, {"firstname": "Maxime", "surname": "Beugnet"})
+
 }
