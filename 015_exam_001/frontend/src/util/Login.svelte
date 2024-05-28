088dc726a1bdb9525fad8a058d7b6459cfbb4193
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-05-28 22:12:01 +0200

    added enter key function and debugging

--- 015_exam_001/frontend/src/util/Login.svelte
+++ 015_exam_001/frontend/src/util/Login.svelte
@@ -1,38 +1,51 @@
 <script>
+	import { onMount } from "svelte";
     import { userLoginStatus } from "../stores/userLoginStatus.js";
 	import toast, {Toaster} from "svelte-french-toast";
-    import { BASE_URL } from "../stores/generalStore.js";
-
+    import { BASE_URL, debug } from "../stores/generalStore.js";
+	
 	let userNameInput = "re";
 	let userPasswordInput = "re";
 
 	import { fetchPost } from "../stores/fetchStore.js";
 
 	async function login(){ 
+		if (!checkInputValidity(1)) {
+			toast.error("Name and password required.");
+			return -1;
+		}
 		toast("Attempting to log in...");
-		if (!checkInputValidity(1)) return -1;
 		let user = {
 			name: userNameInput,
 			password: userPasswordInput
 		};
-		let urlEnd = $BASE_URL + "/login"; 
-		const result = await fetchPost(user, urlEnd);
+		let url = $BASE_URL + "/login"; 
+		const result = await fetchPost(url, user);
 		let response = await result.json();
 		if (response.username) {
-			toast.success("Welcome back, user!");
+			toast.success(`Welcome back, ${response.username}!`);
 			$userLoginStatus = true;
 		} else {
-			console.log("NOT OK in loginsvelte!"); 
+			if ($debug) console.log("NOT OK in loginsvelte!"); 
 			toast.error("Login not succesful.");
 		}
-	}
+	};
 
 	function checkInputValidity(controlArg) {
 		switch(controlArg) {
 			case 1: return userNameInput.length >= 1 && userPasswordInput.length >= 1;
 			default: return false; 
 		}
-	}
+	};
+
+	onMount(() => {
+		document.getElementById("usernameInput").addEventListener("keypress", (e) => {
+            if (e.key === "Enter") { login() }
+        })
+		document.getElementById("userpasswordInput").addEventListener("keypress", (e) => {
+            if (e.key === "Enter") { login() }
+        })
+	})
 
 </script>
 
