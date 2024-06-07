ce5df1dd1dbc4eef264319a05e03f1330c96e533
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2024-06-07 02:01:25 +0200

    added basic authentication

--- 015_exam_001/backend/routers/auth/authRouter.js
+++ 015_exam_001/backend/routers/auth/authRouter.js
@@ -1,19 +1,19 @@
-let debug = process.argv.includes("debug");
 import "dotenv/config";
-const BASE_URL = process.env.BASE_URL;
 
 import passwordUtil from "../../util/password.js";
 import { Router } from "express";
 import { getUser } from "../../database/users/getUser.js";
+import { createUser } from "../../database/users/createUser.js";
+import { updateUserLastLogonTimeId } from "../../database/users/updateUser.js";
 import mailer from "../../util/mailer.js";
-import { cl, cle } from "../../util/logger.js";
-import { fetchPatch, fetchPatchNoData, fetchPost, fetchGet } from "../../util/fetchUtil.js";
 
 const router = Router();
 const startUpMessage = "Auth Router online.";
 
-import newDataEntryIdNumber from "../../util/usersDatabaseUUID.js";
+import newDataEntryIdNumber from "../../util/usersDatabaseUUID.js"; //TODO btyug UUIDs ist for
 let highestUserIDNumber = newDataEntryIdNumber; //I cannot see how this would have any side-effects. *nervous laughing*
+
+router.use(isAdmin);
 
 router.post("/newusersignup", async (req, res) => {
     const data = req.body;
@@ -25,47 +25,58 @@ router.post("/newusersignup", async (req, res) => {
         password: pw,
         email: data.email,
         signUpDate: new Date().toLocaleString("da-DK", {timeZone: "Europe/Copenhagen"}),
-        lastLogon: null,
+        lastLogon: null, //TODO: brug login -- konssistent terminologi
     };
-    
-    // await fetchPost(newUser, BASE_URL + "/api/users/");
-    await fetchPost("http://localhost:8080/api/users/", newUser);
-
-    req.session.user = newUser;
     try {
-        let newUserEmail = newUser.email;
-        mailer(newUserEmail) 
+        await createUser(newUser);
     } catch (err) {
-        cle(err, "authRouter error in mailing, error:");
+        console.error("authRouter newuserSignup error in createUser:", err);
     }
-    req.session.user = newUser; //try this 3005 
-    res.status(200).send({ data: newUser });
-    
+    try {
+        const newUserEmail = newUser.email;
+        mailer(newUserEmail); 
+    } catch (err) {
+        console.error("authRouter error in mailing, error:", err);
+    }
+    req.session.user = newUser;  
+    res.status(200).send({ data: newUser }); //TODO bare send user
 });
 
 router.post("/login", async (req, res) => {
     const signInattempt = req.body;
     let dbLookup;
     try {
-        dbLookup = await getUser(req.body); //TODO replace this direct call to db with a fetch to userapi
+        dbLookup = await getUser(req.body); 
     } catch (err) {
-        cle(err, "login in authRouter gives error on dbLookUp, error:");
+        console.error("login in authRouter gives error on dbLookUp, error:", err);
     }
     let pwCheck = false;
     try {
         pwCheck = await passwordUtil.compare(signInattempt.password, dbLookup.user.password);
     } catch (err) {
-        cle(err, "pwcheck in authRouter gives error on pwCheck, error:");
+        console.error("pwcheck in authRouter gives error on pwCheck, error:", err);
     }
     if (pwCheck) { 
         req.session.user = dbLookup.user;
         let idNumber = dbLookup.user.id; 
-        let resultOfFetch = await fetchPatchNoData(BASE_URL + "api/users/" + idNumber + "/updateuserlastlogon");
+        const newDate = new Date().toLocaleString("da-DK", {timeZone: "Europe/Copenhagen"});
+        updateUserLastLogonTimeId(idNumber, newDate)
         res.status(200).send({ username: dbLookup.user.username }); //NB: this MUST send response.username back to login.svelte
     } else {
         res.status(403).send({ data: "unlawful password or username"});
     }
 })
+
+export function isAdmin(req, res, next) {
+    let isAdmin = req.session?.role?.admin; //gives undefined if not
+    let session = req.session;
+    console.log("authRouter isAdmin being hit");
+    console.log("isAdmin:", isAdmin);
+    console.log("session:", session);
+    //tjek om bruger har admin rolle i sin session
+    //hvis ja, next ham -- ellers deny
+    next();
+}
 
 console.log(startUpMessage);
 export default router;
